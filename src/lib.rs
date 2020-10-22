dd9a988d35451008e788449202f89f292994191c
municorn <municorn@musicaloft.com>
2020-10-21 18:56:31 -0600

    (lib): Two changes
    -   Add `prompts` mod
    -   Make `Formatting` fields `pub`

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,23 @@
 use ansi_term::Style;
 use std::io::{stdin, stdout, Write};
 
-pub mod spans;
 pub mod data;
 pub mod errors;
+pub mod prompts;
 pub mod schedule;
+pub mod spans;
 pub mod task;
 
-pub use spans::*;
 pub use data::*;
 pub use errors::SparrowError;
 pub use schedule::Schedule;
+pub use spans::*;
 pub use task::Task;
 
 pub struct Formatting {
-    prompt: Style,
-    prompt_format: Style,
-    error: Style,
+    pub prompt: Style,
+    pub prompt_format: Style,
+    pub error: Style,
 }
 
 /// a fancy bool. deal with it

f79cec16245fcaf0698bc50ef31c5b4836652b7b
municorn <municorn@musicaloft.com>
2020-11-01 21:31:28 -0700

    prompts: Various changes
    -   Trim new lines off the end of user input
    -   Add `prompt_datetime`

--- src/prompts.rs
+++ src/prompts.rs
@@ -1,3 +1,6 @@
+use chrono::prelude::*;
+
+use crate::errors::SparrowResult;
 use crate::{Formatting, SparrowError};
 use std::io::{stdin, stdout, Write};
 
@@ -47,6 +50,7 @@ fn get_input() -> Result<String, SparrowError> {
     let mut s = String::new();
     stdout().flush()?;
     stdin().read_line(&mut s)?;
+    s = s.trim_end_matches('\n').to_string();
     Ok(s)
 }
 
@@ -93,5 +97,47 @@ pub fn prompt_yn(prompt_string: &str) -> Result<Option<Decision>, SparrowError> 
         } else {
             print!("(What?)  ");
         }
+    }
+}
+
+pub fn prompt_datetime(
+    formatting: &Formatting,
+    date_format: &str,
+    time_format: &str,
+    allow_midnight_on_empty: bool,
+) -> SparrowResult<DateTime<Local>> {
+    let time_prompt_format = if allow_midnight_on_empty {
+        format!("{}, or empty for midnight", time_format)
+    } else {
+        time_format.to_string()
+    };
+
+    let date = prompt_strict(formatting, "Date?", Some(&date_format), |i| {
+        NaiveDate::parse_from_str(i.trim(), date_format)
+    })?;
+    let time_opt = prompt_strict(formatting, "Time?", Some(&time_prompt_format), |i| {
+        if i.is_empty() && allow_midnight_on_empty {
+            Ok(None)
+        } else {
+            NaiveTime::parse_from_str(i, &time_format).map(Some)
+        }
+    })?;
+
+    // determine date/time from user input
+    if let Some(time) = time_opt {
+        if let Some(d) = Local.from_local_datetime(&date.and_time(time)).earliest() {
+            Ok(d)
+        } else {
+            Err(SparrowError::BasicMessage(
+                "Sorry, the time you entered can't be converted to your local timezone."
+                    .to_string(),
+            ))
+        }
+    } else if let Some(d) = Local.from_local_datetime(&date.and_hms(0, 0, 0)).earliest() {
+        Ok(d)
+    } else {
+        Err(SparrowError::BasicMessage(
+            "Sorry, the date you entered can't be converted to your local timezone.".to_string(),
+        ))
     }
 }
